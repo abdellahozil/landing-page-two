36d00d4ea5dbf4910df96a146eb9612c7fcf9bdf
abdellahozil <abdellah11ozil@gmail.com>
2025-11-21 22:37:54 +0100

    change index.html

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/landing-page-one/",
+  base: "/landing-page-two/",
   plugins: [react(),tailwindcss()],
 })
