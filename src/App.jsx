278173bc4ed78ca4f5a6686fae0982bddc9499aa
abdellahozil <abdellah11ozil@gmail.com>
2025-11-22 01:32:33 +0100

    delete react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import Home from "./pages/Home"
-// import NotFound from "./pages/NotFound"
+
 
 function App() {
   
 
   return (
     <>
-      <BrowserRouter basename='/landing-page-two'>
-       <Routes>
-        <Route index element={<Home/>}></Route>
-       </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   )
 }
